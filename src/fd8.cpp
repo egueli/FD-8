7f763abad6dc8aa0eea844727871dc02d884bbaa
Enrico Gueli <enrico.gueli@gmail.com>
2025-04-02 18:38:39 +0200

    add debug pin for scope triggering

--- src/fd8.cpp
+++ src/fd8.cpp
@@ -41,6 +41,7 @@ struct spi_pins {
 	DEFINE_PIN( clk,  B, 2);
 };
 DEFINE_PIN( select_potmeter, B, 1);
+DEFINE_PIN(debug, B, 3);
 
 namespace
 {
@@ -60,6 +61,8 @@ namespace
 
 int main()
 {
+	make_output(debug);
+	set(debug);
 
 	set( select_potmeter);
 	spi::init();
@@ -74,7 +77,9 @@ int main()
 	for(;;)
 	{
 		_delay_ms( 1);
+		reset(debug);
 		uint8_t val = mapper.read_scaled_pedal(adc);
 		write_pot( val);
+		set(debug);
 	}
 }
